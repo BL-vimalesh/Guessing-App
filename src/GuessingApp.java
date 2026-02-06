1b633ae85d6a491dad0485ccf9e17d62d89be035
vimalesh <vimalesh.ravikumar@bridgelabz.com>
2026-02-06 11:25:30 +0530

    Added GuessingApp uc-1

---
+++ src/GuessingApp.java
@@ -0,0 +1,7 @@
+public class GuessingApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Guess App");
+        GameConfig guess = new GameConfig();
+        guess.show();
+    }
+}
